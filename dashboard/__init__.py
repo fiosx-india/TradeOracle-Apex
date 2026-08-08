8c32eda07555556a01d39f2abc6439a1e001c488
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-08-09 01:55:59 +0530

    Update dashboard package description and exports

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -1,9 +1,11 @@
-"""TradeOracle Apex dashboard layer."""
+"""TradeOracle Apex dashboard package."""
 
 from .dashboard import Dashboard
 from .movement_alert import MovementAlert
+from .commodity_view import CommodityView
 
 __all__ = [
     "Dashboard",
     "MovementAlert",
+    "CommodityView",
 ]
